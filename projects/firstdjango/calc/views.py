acdae26595467a4f955b5acaa1d70e8a6bdf21d5
unknown <aishwaryaaj2002@gmail.com>
2020-12-01 21:17:13 +0530

    initial commit

---
+++ projects/firstdjango/calc/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+   # return HttpResponse("Hello World")
+    return render(request,'home.html',{'display':' Everyone'})
+
+def add(request):
+   res=int(request.POST['num1']) + int(request.POST['num2'])
+   return render(request,'result.html',{'result':res})
